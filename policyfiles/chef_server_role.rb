6f8abb8a982db00dbdff98b0e55f4c4abd8b0a0a
Corey Hemminger <hemminger@hotmail.com>
2022-08-12 14:54:53 -0500

    add ci/cd pipelines and policyfiles (#1)

---
+++ policyfiles/chef_server_role.rb
@@ -0,0 +1,14 @@
+# Policyfile.rb - Describe how you want Chef Infra Client to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://docs.chef.io/policyfile.html
+
+# A name that describes what the system you're building with Chef does.
+name 'chef_server_role'
+
+# Where to find external cookbooks:
+default_source :supermarket, 'https://chef-supermarket.home.com'
+default_source :supermarket
+
+# run_list: chef-client will run these recipes in the order specified.
+run_list 'chef_server_role::default'
