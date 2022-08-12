bf6174503c07f396bcf38f3eff16ef95a1a0b569
Corey Hemminger <hemminger@hotmail.com>
2022-08-12 17:49:58 -0500

    add base_config and base_role
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

---
+++ policyfiles/base_role.rb
@@ -0,0 +1,14 @@
+# Policyfile.rb - Describe how you want Chef Infra Client to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://docs.chef.io/policyfile/
+
+# A name that describes what the system you're building with Chef does.
+name 'base_role'
+
+# Where to find external cookbooks:
+default_source :supermarket, 'https://chef-supermarket.home.com'
+default_source :supermarket
+
+# run_list: chef-client will run these recipes in the order specified.
+run_list 'base_role::default'
